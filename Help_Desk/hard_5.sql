ae29c345a0e564d39cf2e5a3be92917e25213065
Fardin Hossain <fardinoyon123@gmail.com>
2023-10-18 14:56:11 +0600

    Update hard_5.sql

--- Help_Desk/hard_5.sql
+++ Help_Desk/hard_5.sql
@@ -1,3 +1,5 @@
+-------------------- NOT SOLVED YET -------------------------
+
 SELECT  Taken_by, 
         Call_date,
         -- TIMESTAMPDIFF(MINUTE, LAG(Call_date,1) OVER (PARTITION BY Taken_by ORDER BY Call_date), Call_date) AS minute_diff,
